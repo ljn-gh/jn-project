8ecb3df15ea44589689ef63f0565cab9f003ebeb
ljn-gh <ljn_email@163.com>
2024-10-21 11:26:54 +0800

    项目结构调整

---
+++ demo-module/src/main/java/com/jianan/demomodule/config/WebSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.jianan.demomodule.config;
+
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Author: jn
+ * @Date: 2024/10/15
+ * @description
+ **/
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    
+}
